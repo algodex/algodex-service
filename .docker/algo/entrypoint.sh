ec92d6dc9427488f39714d64a9cb7c9393214a1c
Michael Feher <git@phearzero.com>
2021-10-15 22:14:58 -0500

    🐳 Adds local overrides and ENV Variables

--- .docker/algo/entrypoint.sh
+++ .docker/algo/entrypoint.sh
@@ -6,7 +6,7 @@
 # Testnet: https://algorand-catchpoints.s3.us-east-2.amazonaws.com/channel/testnet/latest.catchpoint
 # Mainnet: https://algorand-catchpoints.s3.us-east-2.amazonaws.com/channel/mainnet/latest.catchpoint
 
-./goal node catchup 17240000#ANXS5ZTSXG7MOGLJIISRTAQE5V45XTYEDSMSALKFMQQ5QYDELIBQ -d $ALGORAND_DATA
+#./goal node catchup 17240000#ANXS5ZTSXG7MOGLJIISRTAQE5V45XTYEDSMSALKFMQQ5QYDELIBQ -d $ALGORAND_DATA
 
 ./goal node status -w 1000
 
