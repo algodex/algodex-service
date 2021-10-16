bb0fd45fd78f567a2a9dbac168f259fe9911c6c7
Michael Feher <git@phearzero.com>
2021-10-15 22:10:29 -0500

    🎨 Cleanup entrypoint

--- server.js
+++ server.js
@@ -2,16 +2,8 @@
  * Server configures the Queues and starts a Service based on Context
  */
 require('dotenv').config();
-const Redis  = require('ioredis');
-const PouchDB = require("pouchdb-core");
-const Queue = require('bullmq').Queue;
 
-// Setup connections to Redis
-const queue = new Redis(process.env['REDIS_MQ_ADDRESS'] || 6379, process.env['REDIS_MQ_ADDRESS'] || "queue");
-const events  = new Redis(process.env['REDIS_ADDRESS'] || "events");
-
-const dbUrl = 'http://admin:dex@couchdb:5984/dex'
-
+// Configure database
 const getDatabase = require('./src/db');
 const db = getDatabase();
 
@@ -19,9 +11,12 @@ const db = getDatabase();
 const getQueues = require('./src/queues');
 const queues = getQueues();
 
+const getEvents = require('./src/events');
+const events = getEvents();
+
 // Context Switcher, Set APP_CONTEXT to run individual services
 if(typeof process.env['APP_CONTEXT'] === 'undefined'){
     process.env['APP_CONTEXT'] = 'socket'
 } else {
-    require(`./services/${process.env['APP_CONTEXT']}`)({queues, db})
+    require(`./services/${process.env['APP_CONTEXT']}`)({events, queues, db})
 }
