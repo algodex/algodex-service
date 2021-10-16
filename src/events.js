e805d23cf94498715673f7bb83529d1b0e4258fa
Michael Feher <git@phearzero.com>
2021-10-15 22:11:27 -0500

    🔧 Update ENV Variables

--- src/events.js
+++ src/events.js
@@ -6,7 +6,7 @@ let events;
 module.exports = function() {
     if(typeof events === 'undefined'){
         // Define connection
-        events  = new Redis(process.env['REDIS_ADDRESS'] || "events");
+        events  = new Redis(parseInt(process.env['REDIS_PORT'] || 6379), process.env['REDIS_ADDRESS'] || "events");
     }
     return events;
 }
