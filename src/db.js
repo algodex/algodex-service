fbf831568190036338ee5c0b34499084aeb17a9f
Michael Feher <git@phearzero.com>
2021-10-15 22:12:52 -0500

    🎨 Cleanup Database Instance, Use ENV Variables

--- src/db.js
+++ src/db.js
@@ -3,12 +3,7 @@ PouchDB.plugin(require('pouchdb-adapter-http'));
 PouchDB.plugin(require('pouchdb-find'));
 let db;
 
-PouchDB.prototype.getExistingBlocks = async function (start){
-    return await this.find({
-        selector: {_id: {'$gte': start}},
-    })
-}
-let url = process.env['COUCHDB_URL'] || 'http://admin:dex@localhost:5984/dex';
+const url = process.env['COUCHDB_URL'] || 'http://admin:dex@couchdb:5984/dex';
 // Singleton factory pattern for Database
 module.exports = function() {
     if(typeof db === 'undefined'){
