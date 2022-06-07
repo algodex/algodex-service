7cd06ce10556dce535d2fc44791dd46482180e83
Alexander Trefonas <atrefonas@gmail.com>
2022-06-07 11:15:28 -0400

    Get account info from dirty escrows

--- services/worker.js
+++ services/worker.js
@@ -1,5 +1,42 @@
 const bullmq = require('bullmq');
 const Worker = bullmq.Worker;
+const algosdk = require('algosdk');
+
+let indexerClient = null;
+
+const getDirtyAccounts = (block) => {
+  console.log( {block} );
+  if (block.txns === undefined) {
+    return [];
+  }
+  const txnTypes = ['snd', 'rcv', 'close', 'asnd', 'arcv', 'aclose', 'fadd'];
+  const dirtyAccounts = block.txns.reduce( (accounts, txn) => {
+    txnTypes.forEach( (type) => {
+      if (txn.txn !== undefined && txn.txn[type] !== undefined) {
+        const account = txn.txn[type];
+        accounts[account] = 1;
+      }
+    });
+    return accounts;
+  }, {});
+  return Object.keys(dirtyAccounts);
+};
+
+const initOrGetIndexer = () => {
+  if (indexerClient !== null) {
+    return indexerClient;
+  }
+  const algosdk = require('algosdk');
+  const baseServer = "https://testnet-algorand.api.purestake.io/idx2";
+  const port = "";
+  
+  const token = {
+      'X-API-key': 'VELyABA1dGqGbAVktbew4oACvp0c0298gMgYtYIb',
+  }
+  
+  indexerClient = new algosdk.Indexer(token, baseServer, port);
+  return indexerClient;
+}
 
 module.exports = ({queues, db, escrowDB}) =>{
   // Lighten the load on the broker and do batch processing
@@ -17,15 +54,41 @@ module.exports = ({queues, db, escrowDB}) =>{
             ...response,
           });
 
+          // eslint-disable-next-line max-len
+          //const dirtyAccounts = getDirtyAccounts(job.data).map( (account) => [account] );
+          const dirtyAccounts = [['NCL6MAVCMFKRM7NHOZZX3ZK7HBR52CD2UEZI5M3TYNAFUQUREJCRD5CALI']];
+          //const dirtyAccount = '["ZKJV3VOLBC7E4ZRXCZALGYZ5DS7VGN7EGTBKIBKJLYE3MNQ5GKSZNYRL7E"]';
+          //console.log({dirtyAccounts});
           const data = {'adasdas': 'bbbbb'};
           console.log('here55');
-          db.query('dex/orders').then(function(res) {
-            // got the query results
-            console.log('asdasdas');
-            console.log ({res});
-          }).catch(function (err) {
-            console.log({err});
-          });
+          db.query('dex/orders',
+              {reduce: true, group: true, keys: dirtyAccounts})
+              .then(function(res) {
+                if (!res?.rows?.length) {
+                  return;
+                }
+                res.rows.forEach( (row) => {
+                  const account = row.key[0];
+                  const indexerClient = initOrGetIndexer();
+                  const round = job.data.rnd;
+                  const accountInfoPromise = indexerClient.lookupAccountByID(account).round(round).do();
+                  accountInfoPromise.then(function(res) {
+                    console.log(res);
+                  }).catch(function (err) {
+                    console.log({err});
+                  });
+                  //.then(function (res) {
+                    //console.log({accountInfo});
+
+                  //});
+                });
+                // got the query results
+                console.log('found dirty escrow! '+ res.rows[0]);
+
+                console.log ({res});
+              }).catch(function (err) {
+                console.log({err});
+              });
           console.log('here57');
           escrowDB.post({_id: `${job.data.rnd}`, type: 'block', data: data})
               .then(function(response) {
