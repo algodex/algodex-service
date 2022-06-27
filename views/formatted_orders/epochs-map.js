a5b30e62beb73cd05fe5840229725b8babf94679
Alexander Trefonas <atrefonas@gmail.com>
2022-06-27 12:36:36 -0400

    Fix indent

--- views/formatted_orders/epochs-map.js
+++ views/formatted_orders/epochs-map.js
@@ -1,42 +1,42 @@
 module.exports = function(doc) {
     const unixToEpoch = (unixTime) => {
-    let rounded = Math.floor(unixTime);
-    let start = 1629950400 // start time. change mainnet or testnet
-    const secondsInEpoch = 604800 * 2;
+        let rounded = Math.floor(unixTime);
+        let start = 1629950400 // start time. change mainnet or testnet
+        const secondsInEpoch = 604800 * 2;
         return Math.floor((rounded - start) / secondsInEpoch) + 1;
     };
 
     const getEpochFromEscrow = (escrow) => {
-    const history = escrow.history;
-    let startTime = null;
-    let endTime = null;
-    const epochSet = {};
-    for (let i = 0; i < history.length; i++) {
-        const hasBalance = history[i].asaAmount > 0 || history[i].algoAmount > 0;
-        if (startTime === null && hasBalance) {
-            startTime = history[i].time;
-        } else if (startTime !== null && !hasBalance) {
-            endTime = history[i].time;
-            const startEpoch = unixToEpoch(startTime);
-            const endEpoch = unixToEpoch(endTime);
-            const epochCount = endEpoch - startEpoch + 1;
-            const epochs = Array(epochCount).fill().map((element, index) => index + startEpoch);
-            epochs.forEach(epoch => epochSet[`epoch:${epoch}`] = 1);
-            startTime = null;
-            endTime = null;
+        const history = escrow.history;
+        let startTime = null;
+        let endTime = null;
+        const epochSet = {};
+        for (let i = 0; i < history.length; i++) {
+            const hasBalance = history[i].asaAmount > 0 || history[i].algoAmount > 0;
+            if (startTime === null && hasBalance) {
+                startTime = history[i].time;
+            } else if (startTime !== null && !hasBalance) {
+                endTime = history[i].time;
+                const startEpoch = unixToEpoch(startTime);
+                const endEpoch = unixToEpoch(endTime);
+                const epochCount = endEpoch - startEpoch + 1;
+                const epochs = Array(epochCount).fill().map((element, index) => index + startEpoch);
+                epochs.forEach(epoch => epochSet[`epoch:${epoch}`] = 1);
+                startTime = null;
+                endTime = null;
+            }
         }
-    }
-    const lastIndex = history.length - 1;
-    let stillOpen = false;
-    if (history[lastIndex].asaAmount > 0 || history[lastIndex].algoAmount > 0) {
-        const lastEpoch = unixToEpoch(history[lastIndex].time);
-        epochSet[`epoch:${lastEpoch}`] = 1
-        stillOpen = true;
-    }
-    return {
-        epochs: Object.values(epochSet),
-        stillOpen
-    };
+        const lastIndex = history.length - 1;
+        let stillOpen = false;
+        if (history[lastIndex].asaAmount > 0 || history[lastIndex].algoAmount > 0) {
+            const lastEpoch = unixToEpoch(history[lastIndex].time);
+            epochSet[`epoch:${lastEpoch}`] = 1
+            stillOpen = true;
+        }
+        return {
+            epochs: Object.values(epochSet),
+            stillOpen
+        };
     }
 
     if (doc.data === undefined) {
