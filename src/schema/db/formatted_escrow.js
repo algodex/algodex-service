9e7bf6c393669c3e599cba8a81b84e415692227c
Alexander Trefonas <atrefonas@gmail.com>
2022-06-27 19:17:51 -0400

    Improve formatted_escrow schema

--- src/schema/db/formatted_escrow.js
+++ src/schema/db/formatted_escrow.js
@@ -7,8 +7,29 @@ const schema = {
       type: 'object',
       properties: {
         indexerInfo: {...require('./indexed_escrow')()},
+        escrowInfo: {...require('./props/escrow-info')()},
+        lastUpdateUnixTime: {type: 'integer'},
+        lastUpdateRound: {type: 'integer'},
+        assetDecimals: {type: 'integer'},
+        history: {type: 'array', minItems: 1, uniqueItems: true,
+          items: {
+            type: 'object',
+            properties: {
+              algoAmount: {type: 'integer'},
+              asaAmount: {type: 'integer'},
+              time: {type: 'integer'},
+              round: {type: 'integer'},
+            },
+            oneOf: [
+              {required: ['time', 'round', 'algoAmount']},
+              {required: ['time', 'round', 'asaAmount']},
+            ],
+          },
+        },
+
       },
-      required: ['indexerInfo'],
+      required: ['indexerInfo', 'escrowInfo', 'lastUpdateUnixTime',
+        'lastUpdateRound', 'assetDecimals', 'history'],
     },
   },
   required: ['_id', 'data'],
