ec050e65c17af39578d95830d162077406037a35
Alexander Trefonas <atrefonas@gmail.com>
2022-06-29 20:07:25 -0400

    Fix version logic in reducer

--- views/blocks/orders-reduce.js
+++ views/blocks/orders-reduce.js
@@ -1,13 +1,15 @@
 module.exports = function(keys, values, rereduce) {
   const finalOrder = values.reduce(function(finalOrder, order) {
+    const version = order.version || finalOrder.version;
+
     if (order.block > finalOrder.block) {
       finalOrder = order;
     }
-    if (order.version && !finalOrder.version) {
-      finalOrder.version = order.version;
+    if (!finalOrder.version) {
+      finalOrder.version = version;
     }
     return finalOrder;
   }, values[0]);
   finalOrder.status = finalOrder.type === 'open' ? 'open' : 'closed';
   return finalOrder;
-};
+}
