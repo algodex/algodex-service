126fbda8f865c130423f45fb53ea9df023beddc3
Alexander Trefonas <atrefonas@gmail.com>
2022-07-01 15:20:51 -0400

    Add initial balance code

--- src/rewards/get-initial-balances.js
+++ src/rewards/get-initial-balances.js
@@ -1,7 +1,17 @@
 
 const getInitialBalances = (unixTime, escrows) => {
-    throw 'PLEASE IMPLEMENT';
-  };
+  return escrows.reduce( (escrowToBalance, escrow) => {
+    const history = escrow.data.history;
+    let balance = 0;
+    for (let i = 0; i < history.length; i++) {
+      if (history[i].time <= unixTime) {
+        balance = escrow.algoAmount || escrow.asaAmount;
+      } else {
+        break;
+      }
+    }
+    escrowToBalance[escrow] = balance;
+  }, {});
+};
 
 module.exports = getInitialBalances;
-  
