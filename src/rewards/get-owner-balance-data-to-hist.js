a21c4edc860a165f29640e19a20255fa160aa1b0
Alexander Trefonas <atrefonas@gmail.com>
2022-07-01 14:56:52 -0400

    Add getOwnerBalanceDataToHist implementation

--- src/rewards/get-owner-balance-data-to-hist.js
+++ src/rewards/get-owner-balance-data-to-hist.js
@@ -1,11 +1,35 @@
 
-const getOwnerBalanceDataToHist = (ownerBalanceData) => {
-    throw 'PLEASE IMPLEMENT';
-  
-    return ownerBalanceData.rows.reduce( (ownerToHist, row) => {
-      const owner = row;
-      return ownerToHist;
-    }, {});
-  };
+const getDatabases = require('../../src/db/get-databases');
+const databases = getDatabases();
+
+const getOwnerBalanceDataToHist = async (ownerBalanceData) => {
+  const blockSet = ownerBalanceData.rows.reduce(
+      (set, row) => set.add(row.value.block), new Set());
+
+  const blockDB = databases.blocks;
+  const timesData = await blockDB.query('blocks/blockToTime', {
+    keys: Array.from(blockSet)});
+  const blockToTime = timesData.rows.reduce((map, row) => {
+    map[`rnd:${row.key}`] = row.value;
+    return map;
+  }, {});
+
+  return ownerBalanceData.rows.reduce( (ownerToHist, row) => {
+    const owner = row.key;
+    const algxBalance = row.value.balance;
+    const block = parseInt(row.value.block);
+    const time = blockToTime[`rnd:${block}`];
+    if (ownerToHist[owner] === undefined) {
+      ownerToHist[owner] = [];
+    }
+    const entry = {
+      block,
+      time,
+      algxBalance,
+    };
+    ownerToHist[owner].push(entry);
+    return ownerToHist;
+  }, {});
+};
 
 module.exports = getOwnerBalanceDataToHist;
