f17d98acffa0b6ee98a8a82c712389facfbaffe2
Alexander Trefonas <atrefonas@gmail.com>
2022-07-19 17:31:44 -0400

    Fix some sync issues

--- src/sleep-while-waiting-for-queues.js
+++ src/sleep-while-waiting-for-queues.js
@@ -1,8 +1,8 @@
 const getQueueCounts = require('./get-queue-counts');
 const sleep = require('./sleep');
 
-const sleepWhileWaitingforQueues = async (queues) => {
-  while (await getQueueCounts(queues) > 50) {
+const sleepWhileWaitingforQueues = async (queues, limit=50) => {
+  while (await getQueueCounts(queues) > limit) {
     console.log('Sleeping for 200ms while waiting for ' +
       JSON.stringify(queues) + '!');
     await sleep(200);
