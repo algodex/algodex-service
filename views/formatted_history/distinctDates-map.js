491d6af959a4b6966a26fc2392a640b9f1a9e7d3
Alexander Trefonas <atrefonas@gmail.com>
2022-07-20 08:12:49 -0400

    Add distinct dates map

---
+++ views/formatted_history/distinctDates-map.js
@@ -0,0 +1,11 @@
+module.exports = function(doc) {
+  const milliseconds = doc.unixTime * 1000;
+  const dateObject = new Date(milliseconds);
+  const humanDateFormat = dateObject.toLocaleString(); // 2019-12-9 10:30:15
+  const date = humanDateFormat.split(',')[0];
+  const monthYear = date.split('/')[0] + '/' + date.split('/')[2];
+  const owner = doc.tradeType === 'buy' ?
+    doc.assetBuyerAddr : doc.assetSellerAddr;
+  emit(owner+':date', date);
+  emit(owner+':month', monthYear);
+}
