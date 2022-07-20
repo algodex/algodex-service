ef14f774308c4ac4ce2ad25067122f30991a938e
Alexander Trefonas <atrefonas@gmail.com>
2022-07-20 14:07:31 -0400

    getAssetsAndOrdersPromises

--- services/block-worker/getOrdersPromise.js
+++ services/block-worker/getOrdersPromise.js
@@ -17,6 +17,36 @@ const removeEarliestRound = (rows, round) => {
   return newRows;
 };
 
+const getAssetsAndOrdersPromises = ({queues, validRows, blockData}) => {
+  const assetIdSet = {};
+  return validRows.reduce( (allPromises, row) => {
+    const assetId = row.value.assetId;
+    if (!('assetId:assetIds' in assetIdSet)) {
+      assetIdSet[assetId] = 1;
+      const assetAddPromise = getAssetQueuePromise(
+          queues.assets,
+          assetId,
+      );
+      allPromises.push(assetAddPromise);
+    }
+
+    const account = row.key[0];
+
+    const ordersJob = {account: account,
+      blockData: blockData, reducedOrder: row};
+    console.log('queuing order: ' + ordersJob.account + ' ' +
+      ordersJob.blockData.rnd);
+    const promise = queues.orders.add('orders', ordersJob,
+        {removeOnComplete: true}).then(function() {
+    }).catch(function(err) {
+      console.error('error adding to orders queue:', {err} );
+      throw err;
+    });
+    allPromises.push(promise);
+    return allPromises;
+    // //console.log('adding to orders');
+  }, []);
+};
 const getOrdersPromise = ({databases, queues, dirtyAccounts, blockData}) => {
   return databases.blocks.query('blocks/orders',
       {reduce: true, group: true, keys: dirtyAccounts})
@@ -30,7 +60,6 @@ const getOrdersPromise = ({databases, queues, dirtyAccounts, blockData}) => {
         if (!res.rows.length) {
           return;
         }
-        const assetIdSet = {};
         const accountsToVerify = res.rows;
 
         console.log('verifying ' + blockData.rnd,
@@ -40,37 +69,8 @@ const getOrdersPromise = ({databases, queues, dirtyAccounts, blockData}) => {
         console.log('got valid rows: ' + JSON.stringify(validRows));
 
         const assetsAndOrdersPromises =
-          validRows.reduce( (allPromises, row) => {
-          // add job
+          getAssetsAndOrdersPromises({queues, validRows, blockData});
 
-            //console.log('got account', {key});
-
-            const assetId = row.value.assetId;
-            if (!('assetId:assetIds' in assetIdSet)) {
-              assetIdSet[assetId] = 1;
-              const assetAddPromise = getAssetQueuePromise(
-                  queues.assets,
-                  assetId,
-              );
-              allPromises.push(assetAddPromise);
-            }
-
-            const account = row.key[0];
-
-            const ordersJob = {account: account,
-              blockData: blockData, reducedOrder: row};
-            console.log('queuing order: ' + ordersJob.account
-              + ' ' + ordersJob.blockData.rnd);
-            const promise = queues.orders.add('orders', ordersJob,
-                {removeOnComplete: true}).then(function() {
-            }).catch(function(err) {
-              console.error('error adding to orders queue:', {err} );
-              throw err;
-            });
-            allPromises.push(promise);
-            return allPromises;
-            // //console.log('adding to orders');
-          }, []);
         return Promise.all(assetsAndOrdersPromises);
       }).catch(function(err) {
         if (err.error === 'not_found') {
