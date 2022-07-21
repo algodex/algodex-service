179709f13a01ab3ad5ac4bf970414e7077878955
Alexander Trefonas <atrefonas@gmail.com>
2022-07-20 20:46:48 -0400

    add block to db test

--- services/block-worker/addBlockToDB.js
+++ services/block-worker/addBlockToDB.js
@@ -2,15 +2,17 @@ const withSchemaCheck = require('../../src/schema/with-db-schema-check');
 
 const addBlockToDB = async (blocksDB, round, blockData) => {
   try {
-    await blocksDB.get(`${round}`);
+    return await blocksDB.get(`${round}`);
   } catch (e) {
     if (e.error === 'not_found') {
       try {
-        await blocksDB.post(withSchemaCheck('blocks', {_id: `${round}`,
-          type: 'block', ...blockData}));
+        const result =
+          await blocksDB.post(withSchemaCheck('blocks', {_id: `${round}`,
+            type: 'block', ...blockData}));
         console.debug({
           msg: `Block stored`, round: `${round}`,
         });
+        return result;
       } catch (err) {
         if (err.error === 'conflict') {
           console.error('already added! Still not supposed to happen');
