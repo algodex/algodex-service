227f2fc885bd3ceb06616098f4afff47ee535a62
Alexander Trefonas <atrefonas@gmail.com>
2022-09-07 11:29:28 -0400

    remove extra block workers

--- bin/start-services.sh
+++ bin/start-services.sh
@@ -3,8 +3,6 @@
 mkdir -p log
 APP_CONTEXT=asset-worker pm2 -l ./log/asset-worker.log --name asset-worker start built/server.js
 APP_CONTEXT=block-worker pm2 -l ./log/block-worker.log --name block-worker start built/server.js
-APP_CONTEXT=block-worker pm2 -l ./log/block-worker2.log --name block-worker2 start built/server.js
-APP_CONTEXT=block-worker pm2 -l ./log/block-worker3.log --name block-worker3 start built/server.js
 APP_CONTEXT=formatted-order-worker pm2 -l ./log/formatted-order-worker.log --name formatted-order-worker start built/server.js
 APP_CONTEXT=order-worker pm2 -l ./log --name order-worker -l ./log/order-worker.log start built/server.js
 APP_CONTEXT=trade-history-worker pm2 -l ./log/trade-history-worker.log --name trade-history-worker start built/server.js
