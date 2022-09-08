932aa36cf8656ec8bb0df7dd28cec193525556fc
Alexander Trefonas <atrefonas@gmail.com>
2022-09-08 17:14:53 -0400

    Add code to vest rewards

--- src/rewards/distribute/vest-distribute-rewards.ts
+++ src/rewards/distribute/vest-distribute-rewards.ts
@@ -21,23 +21,78 @@ const getEpochKey = (accrualNetwork, epoch) => {
   return `${accrualNetwork}:${epoch}`;
 };
 
-// export interface DistributeRewardsInput {
-//   algodClient: algosdk.Algod,
-//   rewardsDB: PouchDB,
-//   wallets: Array<string>,
-//   epoch: number,
-//   distributeNetwork: string,
-//   accrualNetwork: string,
-//   fromAccount: algosdk.Account,
-//   sendAssetId: number,
-//   indexer: algosdk.Indexer,
-// }
-
 interface RewardsKey {
   ownerWallet: string,
   assetId: number,
   epoch: number
 }
+
+const addDistributionToDB = async(result:string, distribution: SendRewardsObject, transactionId: string, error?: string) => {
+  const vestedUnixTime = Math.round((new Date()).getTime() / 1000);
+
+  const {
+    algodClient, toWalletAddr, amount, epoch, accrualNetwork, fromAccount, assetId,
+  } = distribution;
+    // eslint-disable-next-line camelcase
+  const id = `${toWalletAddr}:${epoch}:${assetId}:${vestedUnixTime}`;
+    const dbItem:VestedRewards = {
+      ownerWallet: toWalletAddr,
+      vestedRewards: amount,
+      formattedVestedRewards: amount / (10**6),
+      assetId,
+      epoch,
+      vestedUnixTime,
+      fromWallet: fromAccount.addr,
+      result: result,
+      transactionId,
+      _id: id,
+    };
+    if (error) {
+      dbItem.error = error;
+      if (!dbItem.error.includes('missing from')) {
+        console.error(dbItem.error);
+      }
+    }
+    try {
+      await databases.vested_rewards.post(withDbSchemaCheck('rewards_distribution', dbItem));
+    } catch (e) {
+      console.error(e);
+      console.error('Attempted to save: ' + JSON.stringify(dbItem));
+    }
+}
+
+const distributeRewards = async (input:DistributeRewardsInput) => {
+  const plannedDistributions = await getPlannedDistributions(input);
+  await checkHasNeededAlgx(input.fromAccount, input.indexer, plannedDistributions);
+  
+  const sendPromises = plannedDistributions.map(distribution => {
+    sendRewards(distribution).then(res => addDistributionToDB(res.result, res.distribution, res.error));
+  });
+
+  await Promise.all(sendPromises);
+}
+
+const checkHasNeededAlgx = async (fromAccount:algosdk.Account, indexer:algosdk.Indexer,
+  plannedDistributions:Array<PlannedDistribution>):Promise<boolean> => {
+  const accountInfo = await indexer.lookupAccountByID(fromAccount.addr)
+      .includeAll(true).do();
+
+  const algxBalance = getAlgxBalance(accountInfo);
+
+  const totalNeededAlgx = plannedDistributions.reduce((sum, entry) => sum += entry.amount, 0);
+  const algoBalance = accountInfo.account.amount;
+  console.log('Wallet balances: ', {algxBalance, algoBalance});
+  const totalNeededAlgo = plannedDistributions.length * 1000;
+  if (totalNeededAlgx > algxBalance) {
+    // eslint-disable-next-line max-len
+    throw new Error(`Not enough ALGX in wallet! ${totalNeededAlgx} vs ${algxBalance}`);
+  }
+  if (totalNeededAlgo > algoBalance) {
+    // eslint-disable-next-line max-len
+    throw new Error(`Not enough ALGO in wallet! ${totalNeededAlgo} vs ${algoBalance}`);
+  }
+  return true;
+};
 
 
 const getPlannedDistributions = async (input:DistributeRewardsInput): Promise<Array<PlannedDistribution>> => {
@@ -84,7 +139,15 @@ interface SendRewardsObject {
 
 type PlannedDistribution = SendRewardsObject; 
 
-const sendRewards = async (input: SendRewardsObject) => {
+type SendSuccessOrFail = 'success' | 'failure';
+interface SendRewardsResult {
+  result:SendSuccessOrFail,
+  distribution:SendRewardsObject,
+  error?:string,
+  transactionId?:string
+}
+
+const sendRewards = async (input: SendRewardsObject):Promise<SendRewardsResult> => {
   const {fromAccount, toWalletAddr, amount,
     algodClient, accrualNetwork, epoch, assetId} = input;
   const params:SuggestedParams = await ((algodClient.getTransactionParams()) as unknown as JSONRequest).do() as SuggestedParams;
@@ -103,121 +166,11 @@ const sendRewards = async (input: SendRewardsObject) => {
     // Must be signed by the account sending the asset
   const rawSignedTxn = xtxn.signTxn(fromAccount.sk);
   try {
-    await (algodClient.sendRawTransaction(rawSignedTxn) as unknown as JSONRequest).do();
-    return {result: 'success'};
+    const sendResult = await (algodClient.sendRawTransaction(rawSignedTxn) as unknown as JSONRequest).do();
+    return {result: 'success', distribution: input, transactionId: sendResult.txId};
   } catch (e) {
-    return {result: 'failure', error: e.response.text};
+    return {result: 'failure', distribution: input, error: e.response.text};
   }
-};
-
-// /**
-//  *
-//  * @param {Object} input
-//  * @param {Number} input.epoch
-//  * @param {String} input.accrualNetwork
-//  * @param {Object} input.rewardsDB
-//  * @return {Promise} promise
-//  */
-// const getPastDistributionsAccountsSet =
-//   async ({rewardsDB:PouchDB, accrualNetwork:string, epoch:number}) => {
-//     const epochKey = getEpochKey(accrualNetwork, epoch);
-
-//     const accountData =
-//     await rewardsDB.query('rewards_distribution/rewards_distribution', {
-//       keys: [epochKey]});
-//     const wallets = accountData.rows
-//         .map(row => row.value)
-//         .filter(row => row.result === 'success')
-//         .map(row => row.to_wallet);
-//     const pastDistributionsSet = new Set(wallets);
-//     return pastDistributionsSet;
-//   };
-
-
-
-const distributeRewards = async (input:DistributeRewardsInput) => {
-  // const {epoch, distributeNetwork, algodClient, indexer,
-  //   rewardsDB, wallets, fromAccount, accrualNetwork, sendAssetId} = input;
-  // const ajv = new Ajv();
-  // const valid = ajv.validate(schema, input);
-  // if (!valid) {
-  //   throw new Error('Invalid input to distributeRewards! ' +
-  //     JSON.stringify(ajv.errors));
-  // }
-
-  // const accountInfo = await indexer.lookupAccountByID(fromAccount.addr)
-  //     .includeAll(true).do();
-
-  // const algxBalance = getAlgxBalance(accountInfo);
-  // const algoBalance = accountInfo.account.amount;
-  // console.log('Wallet balances: ', {algxBalance, algoBalance});
-  // const totalNeededAlgx = wallets.length * amount;
-  // const totalNeededAlgo = wallets.length * 1000;
-  // if (totalNeededAlgx > algxBalance) {
-  //   // eslint-disable-next-line max-len
-  //   throw new Error(`Not enough ALGX in wallet! ${totalNeededAlgx} vs ${algxBalance}`);
-  // }
-  // if (totalNeededAlgo > algoBalance) {
-  //   // eslint-disable-next-line max-len
-  //   throw new Error(`Not enough ALGO in wallet! ${totalNeededAlgo} vs ${algoBalance}`);
-  // }
-
-  // const pastDistAccountSet =
-  //   await getPastDistributionsAccountsSet({rewardsDB, accrualNetwork, epoch});
-  // console.log(pastDistAccountSet);
-  // const epochKey = getEpochKey(accrualNetwork, epoch);
-
-  // const progressBar =
-  //   new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-  // progressBar.start(wallets.length, 1);
-
-  // for (let i = 0; i < wallets.length; i++) {
-  //   if (globalThis.isGloballyShuttingDown) {
-  //     console.log('\nShutting down early from control+c signal');
-  //     process.exit(0);
-  //   }
-  //   const toWalletAddr = wallets[i];
-  //   progressBar.update(i);
-  //   if (pastDistAccountSet.has(toWalletAddr)) {
-  //     // console.error(`Already sent to ${toWalletAddr}, skipping!`);
-  //     continue;
-  //   }
-  //   const result = await sendRewards({fromAccount, toWalletAddr, amount,
-  //     algodClient, accrualNetwork, epoch, assetId});
-  //   // eslint-disable-next-line camelcase
-  //   const unix_time = Math.round((new Date()).getTime() / 1000);
-  //   // eslint-disable-next-line camelcase
-  //   const id = `${toWalletAddr}:${epochKey}:${unix_time}`;
-  //   const dbItem = {
-  //     to_wallet: toWalletAddr,
-  //     amount,
-  //     assetId,
-  //     epoch,
-  //     accrualNetwork,
-  //     network,
-  //     // eslint-disable-next-line camelcase
-  //     unix_time,
-  //     from_wallet: fromAccount.addr,
-  //     result: result.result,
-  //     _id: id,
-  //   };
-  //   if (result.error) {
-  //     dbItem.error = result.error;
-  //     if (!dbItem.error.includes('missing from')) {
-  //       console.error(dbItem.error);
-  //     }
-  //   }
-  //   try {
-  //     await rewardsDB.post(withDbSchemaCheck('rewards_distribution', dbItem));
-  //   } catch (e) {
-  //     console.error(e);
-  //     console.error('Attempted to save: ' + JSON.stringify(dbItem));
-  //     break;
-  //   }
-  // }
-  // progressBar.update(wallets.length);
-  // console.log('\nFinished sending!');
-  // process.exit(0);
 };
 
 export { distributeRewards, getPlannedDistributions };
