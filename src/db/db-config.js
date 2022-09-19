31e023e16e0011b00094b119f04fd5e156c6ea85
Alexander Trefonas <atrefonas@gmail.com>
2022-09-19 16:33:43 -0400

    add view

--- src/db/db-config.js
+++ src/db/db-config.js
@@ -118,14 +118,14 @@ const databases = [
   {
     dbName: 'view_cache',
     appendOnly: false,
-    // design: {
-    //   _id: '_design/view_cache',
-    //   views: {
-    //     assets: {
-    //       map: require('../../views/assets/assets-map').toString(),
-    //     },
-    //   },
-    // },
+    design: {
+      _id: '_design/view_cache',
+      views: {
+        currentCache: {
+          map: require('../../views/view_cache/currentCache-map').toString(),
+        },
+      },
+    },
   },
   {
     dbName: 'verified_account',
