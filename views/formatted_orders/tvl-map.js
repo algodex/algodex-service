68b0a0e828e09be389b235515101d75692c004f7
Alexander Trefonas <atrefonas@gmail.com>
2022-10-04 12:28:31 -0400

    fix map

--- views/formatted_orders/tvl-map.js
+++ views/formatted_orders/tvl-map.js
@@ -1,8 +1,11 @@
 module.exports = function(doc) {
   const assetId = doc.data.escrowInfo.assetId;
-  const algoAmount = (doc.data.history[0].algoAmount || 0) / (Math.pow(10, 6));
+  const historyLen = doc.data.history.length;
+  const algoAmount =
+    (doc.data.history[historyLen - 1].algoAmount || 0) / (Math.pow(10, 6));
   const decimals = doc.data.assetDecimals;
-  const asaAmount = (doc.data.history[0].asaAmount || 0) / (Math.pow(10, decimals));
+  const asaAmount =
+    (doc.data.history[historyLen - 1].asaAmount || 0) / (Math.pow(10, decimals));
 
   emit(assetId, {algoAmount, asaAmount});
 };
