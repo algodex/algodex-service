546fb5ae4142eea5151dc48d07772fa97a5f4d90
Alexander Trefonas <atrefonas@gmail.com>
2022-10-06 12:31:22 -0400

    Fix eslint

--- views/blocks/orders-reduce.js
+++ views/blocks/orders-reduce.js
@@ -1,5 +1,5 @@
-/* 
- * Algodex Service 
+/*
+ * Algodex Service
  * Copyright (C) 2022 Algodex VASP (BVI) Corp.
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
@@ -13,7 +13,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 
 
 const reducer = function(keys, values, rereduce) {
