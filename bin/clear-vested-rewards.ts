b9bc07a405c2724e304881bbd457a6c833f0ea0a
Alexander Trefonas <atrefonas@gmail.com>
2022-10-06 12:28:29 -0400

    Add copyright

--- bin/clear-vested-rewards.ts
+++ bin/clear-vested-rewards.ts
@@ -1,3 +1,19 @@
+/* 
+ * Algodex Service 
+ * Copyright (C) 2022 Algodex VASP (BVI) Corp.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #!/usr/bin/env node
 
 const getDatabases = require('../src/db/get-databases');
@@ -5,7 +21,7 @@ const getDatabases = require('../src/db/get-databases');
 const clearRewards = async () => {
   console.log("Going to delete all old records from vested_rewards");
   const databases = getDatabases();
-  const db = databases.rewards;
+  const db = databases.vested_rewards;
   const allDocs = await db.allDocs();
   console.log(allDocs.rows.filter(row => row.id.includes(":")).length + " docs to delete");
   const deletePromises = allDocs.rows
--- views/blocks/tradeHistory-map.js
+++ views/blocks/tradeHistory-map.js
@@ -1,3 +1,19 @@
+/* 
+ * Algodex Service 
+ * Copyright (C) 2022 Algodex VASP (BVI) Corp.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 module.exports = function(doc) {
 // Source: http://code.google.com/p/gflot/source/browse/trunk/flot/base64.js?r=153
 // Cached-Source: https://gist.github.com/AndreasMadsen/2693051
