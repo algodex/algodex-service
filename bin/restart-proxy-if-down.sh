00f2e89d99657b3022ce14332b71cc25d1235974
Alexander Trefonas <atrefonas@gmail.com>
2022-11-08 08:20:13 -0500

    set docker id

--- bin/restart-proxy-if-down.sh
+++ bin/restart-proxy-if-down.sh
@@ -5,12 +5,14 @@
 while true
 
 do
-  code=$(curl -s -o /dev/null -w "%{http_code}" 'http://localhost:8000/assets/searchall')
-  if [[ $code -ne 200 ]]; then
+  DOCKER_ID=`sudo docker ps | grep reverse-proxy-rust | grep target | awk '{print $1;}'`
+  
+  CODE=$(curl -s -o /dev/null -w "%{http_code}" 'http://localhost:8000/assets/searchall')
+  if [[ $CODE -ne 200 ]]; then
     echo "Restarting docker for reverse-proxy-rust!"
-    docker restart 60a94f5d4ddf 
+    docker restart "$DOCKER_ID" 
   fi
   echo "Sleeping..."
-  sleep 100 
+  sleep 100
 done
 
