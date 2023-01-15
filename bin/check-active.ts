4567044b95010a52c5c3496b611adae7de0acba3
Alexander Trefonas <atrefonas@gmail.com>
2023-01-15 08:13:19 -0500

    Add script to restart services if broker is not updating

---
+++ bin/check-active.ts
@@ -0,0 +1,56 @@
+import { exec } from 'child_process';
+
+const runExec = (cmd:string):Promise<String> => {
+  console.log(cmd);
+  return new Promise((resolve, reject) => 
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`error: ${error.message}`);
+        reject(`error: ${error.message}`);
+        return;
+      }
+    
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        resolve(`stderr: ${stderr}`);
+        return;
+      }
+    
+      console.log(`stdout:\n${stdout}`);
+      resolve(`stdout:\n${stdout}`);
+    })
+  );
+}
+
+function sleep(ms:number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const checkBrokerActive = async () => {
+  await sleep(1000);  
+  const logText = await runExec('pm2 logs --nostream broker');
+  console.log('here');
+  console.log(logText);
+  const lines = logText.split('\n');
+  const regex = /\s+([0-9]{13})\s*/mg;
+  let lastBlockTime;
+  for (const line of lines) {
+    for (const match of line.matchAll(regex)) {
+      lastBlockTime = match[0];
+    }
+  }
+  const currentTime = Date.now();
+  const secondsSinceBlock = (currentTime/1000) - (lastBlockTime/1000);
+  const maxSeconds = 60;
+  if (secondsSinceBlock > maxSeconds) {
+    console.log('restarting services due to ' + secondsSinceBlock + ' seconds since last block!');
+    await runExec('pm2 stop broker');
+    await sleep(1000);
+    await runExec('pm2 restart all');
+  } else {
+    console.log('seconds is ' + secondsSinceBlock + ' not restarting ' );
+  }
+}
+
+checkBrokerActive();
+
